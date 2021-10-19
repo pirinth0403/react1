abf3423cdb019263dc9430360c7f68b9202d529b
pirinth0403 <pirinthapan1996@gmail.com>
2021-10-19 21:01:34 +0530

    RefsComp

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import FragmentDemo from './components/FragmentDemo';
 import Table from './components/Table';
 import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
+import RefsDemo from './components/RefsDemo';
 
 
 
@@ -38,7 +39,8 @@ function App() {
     {/* <FragmentDemo/>  */}
     {/* <Table/> */} 
     {/* <PureComp />  */} 
-    <ParentComp /> 
+    {/* <ParentComp />  */} 
+    <RefsDemo /> 
     </div>
   );
 }
